017a819138000bc0d57ec1fb7bfe066ebe001af1
YuliiaOst <yuliya.ostrogrud@gmail.com>
2023-05-18 01:04:15 -0500

    Added SDLC file

---
+++ src/YuliiaOst/Sdlc.java
@@ -0,0 +1,23 @@
+package YuliiaOst;
+
+public class Sdlc {
+    /*
+Software Development Life Cycle.
+
+A formalized framework describing activities and steps to develop/build software.
+
+SDLC Steps:
+
+Requirement gathering
+
+Designing
+
+Developing / coding
+
+Testing
+
+Deploying / production
+
+Maintaining
+     */
+}
